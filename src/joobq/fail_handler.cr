c5d61ee59a2e9b369d9063f83343f67ab8e8a2cc
eliasjpr <eliasjpr@gmail.com>
2024-11-13 09:02:10 -0500

    Enhance job failure handling in fail handler
    Introduced additional logging for job expiry and retries processing, improving the transparency of job handling outcomes. The logic for handling expired jobs was added, moving them directly to the Dead Letter Queue, while ensuring retries are accurately tracked and logged. This change provides clearer insight into job status and improves system reliability by distinguishing between expired and failed jobs.

--- src/joobq/fail_handler.cr
+++ src/joobq/fail_handler.cr
@@ -9,14 +9,20 @@ module JoobQ
         backtrace: ex.inspect_with_backtrace[0..10],
         cause:     ex.cause.to_s,
       }
-
       Log.info &.emit("Failed", queue: queue.name, job_id: job.jid.to_s, error: error)
 
-      if job.retries > 0
-        ExponentialBackoff.retry(job, queue)
-      else
-        DeadLetter.add job
+      if job.expires && Time.local > job.expires
+        DeadLetterManager.add(job)
         queue.dead.add(1)
+        Log.error &.emit("Job expired and moved to Dead Letter Queue", job_id: job.jid.to_s)
+      elsif job.retries > 0
+        ExponentialBackoff.retry(job, queue)
+        Log.warn &.emit("Retrying job", queue: queue.name, job_id: job.jid.to_s, retries_left: job.retries)
+        queue.retried.add(1)
+      else
+        DeadLetterManager.add(job)
+        queue.dead.add(1)
+        Log.error &.emit("Job moved to Dead Letter Queue", job_id: job.jid.to_s)
       end
     end
   end
@@ -25,18 +31,11 @@ module JoobQ
     def self.retry(job, queue)
       original_retries = job.retries
       retry_left = original_retries - 1
-
       job.retries = retry_left
       if retry_left > 0
         delay = (2 ** (original_retries - retry_left)) * 1000 # Delay in ms
-
         # Logic to add the job back to the queue after a delay
         queue.store.add_delayed(job, delay)
-        Log.warn &.emit("Retrying job", queue: queue.name, job_id: job.jid.to_s, retries_left: retry_left)
-        queue.retried.add(1)
-      else
-        DeadLetterManager.add(job)
-        queue.dead.add(1)
       end
     end
   end
